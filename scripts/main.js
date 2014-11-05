6fe3b588d07316cb35327625d4b36315f3ab2e56
Khrone <jakub.jafra@gmail.com>
2014-11-05 17:20:57 +0100

    bugfix: AStar not found

--- scripts/main.js
+++ scripts/main.js
@@ -53,6 +53,7 @@ require([	'jquery', 'jquery-mousewheel', 'extend', 'underscore',					// external
 			// Rozwiązaniem jest podzielenie tego na prawidłowe moduły Require,
 			// ale mi się kurwa nie chce.
 			$("head").append('<script src="scripts/logic/tile.js"></script>');
+			$("head").append('<script src="scripts/logic/algorithms.js"></script>');
 			$("head").append('<script src="scripts/logic/product.js"></script>');
 			$("head").append('<script src="scripts/logic/storage.js"></script>');
 			$("head").append('<script src="scripts/logic/country.js"></script>');
